89d70bf0513eef2b0754ed67e218a9214f6d6ae3
vetorL <145482274+vetorL@users.noreply.github.com>
2024-04-16 20:26:05 -0300

    Refatora exercicio17

--- completos/exercicio17.c
+++ completos/exercicio17.c
@@ -16,7 +16,7 @@ typedef struct {
 
 vertice * criaGrafo(int v);
 bool inserirAresta(vertice * g, int i, int j);
-bool existeCaminho(vertice * g, int i, int j);
+void prof(vertice * g, int i, int j, bool * achou);
 
 int main() {
     vertice * g = criaGrafo(V);
@@ -30,7 +30,15 @@ int main() {
     inserirAresta(g, 3, 5);
     inserirAresta(g, 5, 6);
 
-    if(existeCaminho(g, 1, 4)) {
+    bool achou = false;
+
+    // caminho entre 1 e 4
+    int i = 1;
+    int j = 4;
+
+    prof(g, i, j, &achou);
+
+    if(achou) {
         puts("EXISTE CAMINHO");
     } else {
         puts("NAO EXISTE CAMINHO");
@@ -63,25 +71,26 @@ bool inserirAresta(vertice * g, int i, int j) {
     return true;
 }
 
-bool existeCaminho(vertice * g, int i, int j) {
+void prof(vertice * g, int i, int j, bool * achou) {
+    if(*achou) {
+        return;
+    }
+
     g[i].flag = 1;
     no * p = g[i].inicio;
-    bool existe;
 
     while(p) {
-        if(p->adj == j) {
-            return true;
-        }
-
         if(g[p->adj].flag == 0) {
-            existe = existeCaminho(g, p->adj, j);
-            if(existe) return true;
+            if(p->adj == j) {
+                *achou = true;
+                return;
+            }
+
+            prof(g, p->adj, j, achou);
         }
 
-        p=p->prox;
+        p = p->prox;
     }
 
     g[i].flag = 2;
-
-    return false;
 }
